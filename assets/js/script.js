e8d4631f0e6478384b7bd8ad4dff8d0def4044b2
JaredLove <lovejared91@Yahoo.com>
2023-10-11 21:47:58 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,7 +172,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   let taskText = $("#modalTaskDescription").val();
   let taskDate = $("#modalDueDate").val();
@@ -214,16 +214,24 @@ $(".card .list-group").sortable({
   // The activate and deactivate events trigger once for all connected lists as soon as dragging starts and stops.
   activate: function(event) {
     console.log("activate", this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
     console.log("deactivate", this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   // The over and out events trigger when a dragged item enters or leaves a connected list.
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass('dropover-actvie');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass('dropover-actvie');
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   // The update event triggers when the contents of a list have changed (e.g., the items were re-ordered, an item was removed, or an item was added).
   update: function(event) {
@@ -302,7 +310,7 @@ const auditTask = function(taskEl) {
   // convert to moment object at 5:00pm
   const time = moment(date, "L").set("hour", 17);
   // this should print out an object for the value of the date variable, but at 5:00pm of that date
-  console.log(time);
+
 
     // remove any old classes from element
     $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -313,8 +321,15 @@ const auditTask = function(taskEl) {
   }  else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
+// we loop over every task on the page with a class of list-group-item and execute the auditTask() function to check the due date of each one.
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 // load tasks for the first time
 loadTasks();
 
