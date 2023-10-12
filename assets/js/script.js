2d00140af903208a82fe2de727f300faa52a0b99
JaredLove <lovejared91@Yahoo.com>
2023-10-11 19:45:51 -0500

    commits

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,9 @@ let createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+    // check due date
+    auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -33,7 +36,6 @@ let loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -101,30 +103,46 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+    // enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose: function() {
+        // when calendar is closed, force a "change" event on the `dateInput`
+        $(this).trigger("change");
+      }
+    });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   let date = $(this)
     .val()
     .trim();
-
+console.log(date);
   // get the parent ul's id attribute
   let status = $(this)
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-
+console.log(status);
   // get the task's position in the list of other li elements
   let index = $(this)
     .closest(".list-group-item")
     .index();
-
-  // update task in array and re-save to localstorage
+console.log(index);
+console.log(tasks[index]);
+if (tasks[status] && tasks[status][index]) {
+  // Perform the update  
   tasks[status][index].date = date;
+} else {
+  console.error("Task not found.");
+}
+  // update task in array and re-save to localstorage
+
   saveTasks();
 
   // recreate span element with bootstrap classes
@@ -134,6 +152,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+    // Pass task's <li> element into auditTask() to check new due date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -167,7 +188,7 @@ $("#task-form-modal .btn-primary").click(function() {
       text: taskText,
       date: taskDate
     });
-
+    console.log(tasks);
     saveTasks();
   }
 });
@@ -261,6 +282,38 @@ $("#trash").droppable({
     console.log("out");
   }
 });
+
+
+$("#modalDueDate").datepicker({
+  // we've set the minimum date to be one day from the current date.
+  minDate: 1
+});
+
+// jQuery to select the taskEl element and find the <span> element inside it, 
+// then retrieve the text value using .text(). We chained on a JavaScript 
+// (not jQuery) .trim() method to cut off any possible leading or trailing 
+// empty spaces.
+const auditTask = function(taskEl) {
+  // get date from task element
+  const date = $(taskEl).find("span").text().trim();
+  // ensure it worked
+  console.log(date); 
+
+  // convert to moment object at 5:00pm
+  const time = moment(date, "L").set("hour", 17);
+  // this should print out an object for the value of the date variable, but at 5:00pm of that date
+  console.log(time);
+
+    // remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+      // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 // load tasks for the first time
 loadTasks();
